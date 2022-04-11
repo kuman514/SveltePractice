f79958fb63228cfe48bb629e6e6e2d5dd9436547
kumankoishi.py <7iggif@naver.com>
2022-04-11 21:24:34 +0900

    Practicing declarative rendering

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,31 @@
 <script>
 	let name = 'koishi';
+	let num = 514;
+	function assign() {
+		name = 'kuman';
+		num = 55;
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2 class={num < 85 ? 'active' : ''}>
+		{num}
+	</h2>
+	<img src="" alt={name} />
+	<!-- input type="text" value={name} / -->
+	<input type="text" bind:value={name} />
+	<button on:click={assign}>
+		assign
+	</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		color: red;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.active {
+		color: blue;
 	}
 </style>
